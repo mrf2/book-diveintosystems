d7c6ae13c90e8d30fcae3aefcc20cd39336618a8
mrf2 <al.maruf@live.com>
2023-08-24 07:43:31 +0600

    Chapter 03 - Example 07: Debugging Multi-threaded Program

---
+++ ch03/ex06_simple_operations.s
@@ -0,0 +1,48 @@
+	.file	"ex06_simple_operations.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	movl	$1, -8(%ebp)
+	addl	$2, -8(%ebp)
+	subl	$14, -8(%ebp)
+	movl	-8(%ebp), %eax
+	imull	$100, %eax, %eax
+	movl	%eax, -4(%ebp)
+	movl	-4(%ebp), %edx
+	movl	%edx, %eax
+	addl	%eax, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	addl	%eax, -8(%ebp)
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB1:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE1:
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
