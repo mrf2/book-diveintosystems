470aa1027eab5615ef7289f2f9b91f23e0baa5fd
mrf2 <al.maruf@live.com>
2023-08-29 23:53:08 +0600

    Chapter 04 - Example 01: Print example int using both signed and unsigned placeholders

---
+++ ch04/ex01_signed_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int example = -100;
+
+	/* Print example int using both signed and unsigned placeholders.*/
+	printf("%d %u\n", example, example);
+
+	return 0;
+}
+
